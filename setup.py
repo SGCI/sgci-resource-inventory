c62646333fceae39dd39082c5dbabf5e4752a7b5
Daniel Mejia <denphi@denphi.com>
2020-10-30 14:25:02 -0400

    readthedocs build

--- setup.py
+++ setup.py
@@ -3,17 +3,6 @@ from setuptools import setup
 setup(
     name="sgci-resource-inventory",
     install_requires=[
-        'requests',
-        'simplejson',
         'sphinx-jsonschema',
         'msmb_theme'],
-    extras_require={
-        'tests': [
-            'nose',
-            'pycodestyle >= 2.1.0'],
-        'docs': [
-            'sphinx >= 1.4',
-            'sphinx_rtd_theme',
-            'sphinx-jsonschema',
-            'msmb_theme']}
 )
